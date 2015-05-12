575032996bdf55a0fcb7ea730fd0bb326bf61130
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2015-05-12 14:09:54 +0200

    add script for transmission/pushover

---
+++ transmission-done.sh
@@ -0,0 +1,1 @@
+curl -k https://api.pushover.net/1/messages.json -F token=afEbjfq3rR1nFXh8GdyBzf2umnDqYr -F user=u1QS4ssZRRs7nctgv9GHcj6oqEGW8b -F title="Download Complete" -F message="$TR_TORRENT_NAME " -F sound=none -F url=http://192.168.0.8:9091 -F url_title="Visit web interface" 
