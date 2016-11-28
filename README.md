6f0215b27a860898797fdd470287fef958ad02b4
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2016-11-28 11:42:18 +0100

    fix typo in install script

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## installation instructions
 `git clone ssh://git@octotron:2222/linux-config/vim.git ~/.vim/`
 
+read and execute `install.sh`
+
 ## .vimrc
 A custom .vimrc is normally stored at ~/.vimrc, but aparently VIM also
 understands when put here so I can keep it under version control in the
--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ vim +PluginInstall +qall
 mkdir -p ~/.fonts/ && cd "$_"
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 mkdir -p ~/.config/fontconfig/config.d && cd "$_"
-wget https://github.com/powerline/powerline/raw/develop/font/11-powerline-symbols.conf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
