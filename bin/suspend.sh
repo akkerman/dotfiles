5441a6ba1d2ab93c92e409ca6bdb38c67291da53
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-03-05 15:20:29 +0100

    Remove more unused scripts

--- bin/suspend.sh
+++
@@ -1,3 +0,0 @@
-export DISPLAY=:0
-
-sh -c "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock && dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend"
