320f274413da595639e5818cfcbbed1b4cd46d2c
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-03-14 17:33:26 +0100

    Rewrite launch.sh for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +6,22 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar primary &
-polybar secondary &
+PRIMARY_DISPLAY="$(xrandr -q | grep " connected primary" | cut -d' ' -f1)"
+SECONDARY_DISPLAY="$(xrandr -q | grep " connected" | grep -v "primary" | cut -d' ' -f1)"
+
+if [[ -z "$PRIMARY_DISPLAY" ]]; then
+    echo "primary  : NONE"
+else
+    echo "primary  : $PRIMARY_DISPLAY"
+    MONITOR="$PRIMARY_DISPLAY" polybar primary &
+fi
+
+if [ -z $SECONDARY_DISPLAY]; then
+    echo "secondary: NONE"
+else
+    echo "secondary $SECONDARY_DISPLAY"
+    MONITOR="$SECONDARY_DISPLAY" polybar secondary &
+fi
+
 
 echo "Bars launched..."
