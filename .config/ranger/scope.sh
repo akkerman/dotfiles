975a6b2c8dfb853a486c20fd47f8d33473634ace
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-03-14 21:49:40 +0100

    Preview metadata of ebooks in ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -68,6 +68,9 @@ case "$extension" in
         try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         ;; # fall back to highlight/cat if the text browsers fail
+    # ebooks:
+    mobi|epub)
+        try ebook-meta "$path" && { dump ; exit 0; }  || exit 0 ;;
 esac
 
 case "$mimetype" in
