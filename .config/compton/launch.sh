61321ed4afe3653144bc77d1854e61f3b33d5858
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-03-17 09:43:37 +0100

    Add launch script for compton

---
+++ .config/compton/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q compton
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+
+compton --config $HOME/.config/compton/compton.conf
