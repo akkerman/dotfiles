e0570200e2980b185a16d09d250e939071946d78
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-03-21 12:03:43 +0100

    Roll my own lock script

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-ws_empty='.'
+# I'm using feh to set the background image this leaves a .fehbg file in the home directory
+# we can use this file to find one of the images in the background
 
-i3-msg workspace $ws_empty
+# each of the images are put in between single quotes
+# the awk command below splits each line in febg on single quotes
+# making $2 the first image after the feh command
 
-# make screenshot of empty workspace
-scrot /tmp/lock_screen.png
+if [ -f $HOME/.fehbg ]; then
+    # each of the images are put in between single quotes
+    # the awk command below splits each line in febg on single quotes
+    # making $2 the first image after the feh command
+    WALLP=$(awk -F\' '/feh/ {print $2}' $HOME/.fehbg )
 
-# blur the screenshot
-convert /tmp/lock_screen.png -blur 16x9 /tmp/lock_screen.png
+    # xdpyinfo spits out information about the x server
+    # we're interested in the current resolution (dimensions)
+    RESOLUTION=$(xdpyinfo | awk '/dimensions/ {print $2}')
+
+
+    # resize and blur the wallpaper
+    convert "$WALLP" -resize $RESOLUTION! -blur 16x9 /tmp/lock_screen.png
+fi
 
 # lock screen
 i3lock -e -f -c 000000 -i /tmp/lock_screen.png
 
-# restore workspace, so after login not has changed
-sleep 1; i3-msg workspace back_and_forth
+# screen off, music off if i3lock still running after 30 seconds
+sleep 30 && pgrep i3lock && xset dpms force off && playerctl stop
