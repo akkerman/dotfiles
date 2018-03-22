f77bc6c49ade7375eb8e798afc507782c64444ed
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-03-22 09:10:58 +0100

    Add scripts to restore workspace

---
+++ .config/i3/ws/load-2.sh
@@ -0,0 +1,4 @@
+i3-msg "workspace number 2; workspace 2: ; append_layout ~/.i3/workspace-2.json"
+
+(google-chrome --new-window & google-chrome --new-window --app=https://netflix.com/browse/my-list &)
+(termite &)
---
+++ .config/i3/ws/load-7.sh
@@ -0,0 +1,6 @@
+i3-msg "workspace number 7; workspace 7: ; append_layout ~/.i3/workspace-7.json"
+
+(google-chrome --new-window https://inbox.google.com/u/1/ &)
+(/stronghold/download/Telegram/Telegram &)
+(/usr/bin/slack &)
+(termite -e htop &)
