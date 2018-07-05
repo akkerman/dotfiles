9d1524e72cff9f22a5831c58d928179a203b1453
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2018-07-05 11:27:35 +0200

    Change the way we lock the screen

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -7,23 +7,23 @@
 # the awk command below splits each line in febg on single quotes
 # making $2 the first image after the feh command
 
-if [ -f $HOME/.fehbg ]; then
-    # each of the images are put in between single quotes
-    # the awk command below splits each line in febg on single quotes
-    # making $2 the first image after the feh command
-    WALLP=$(awk -F\' '/feh/ {print $2}' $HOME/.fehbg )
+# if [ -f $HOME/.fehbg ]; then
+#     # each of the images are put in between single quotes
+#     # the awk command below splits each line in febg on single quotes
+#     # making $2 the first image after the feh command
+#     WALLP=$(awk -F\' '/feh/ {print $2}' $HOME/.fehbg )
 
-    # xdpyinfo spits out information about the x server
-    # we're interested in the current resolution (dimensions)
-    RESOLUTION=$(xdpyinfo | awk '/dimensions/ {print $2}')
+#     # xdpyinfo spits out information about the x server
+#     # we're interested in the current resolution (dimensions)
+#     RESOLUTION=$(xdpyinfo | awk '/dimensions/ {print $2}')
 
 
-    # resize and blur the wallpaper
-    convert "$WALLP" -resize $RESOLUTION! -blur 16x9 /tmp/lock_screen.png
-fi
+#     # resize and blur the wallpaper
+#     convert "$WALLP" -resize $RESOLUTION! -blur 16x9 /tmp/lock_screen.png
+# fi
 
 # lock screen
-i3lock -e -f -c 000000 -i /tmp/lock_screen.png
+i3lock -t -e -f -c 1b1b1b -i $HOME/.config/i3/lock.png
 
 # screen off, music off if i3lock still running after 30 seconds
 sleep 30 && pgrep i3lock && xset dpms force off && playerctl stop
